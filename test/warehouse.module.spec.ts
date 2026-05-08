1d2fa91aedb05901cde3c7ef0f6b312151838ab4
Eduard Tomášek <tomasek.e@gmail.com>
2026-05-08 18:36:35 +0200

    integration test

--- test/warehouse.module.spec.ts
+++ test/warehouse.module.spec.ts
@@ -1,8 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { WarehouseItem } from '../src/decorators/warehouse-item.decorator';
 import { WAREHOUSE_OPTIONS } from '../src/constants/warehouse.constants';
 import { WarehouseModule } from '../src/warehouse.module';
 import { WarehouseService } from '../src/warehouse.service';
+
+@Injectable()
+class IntegrationLoader {
+  @WarehouseItem({ key: 'countries' })
+  loadCountries() {
+    return { CZ: 'Czech Republic' };
+  }
+}
 
 describe('WarehouseModule', () => {
   it('creates a module with forRoot options', async () => {
@@ -52,5 +61,21 @@ describe('WarehouseModule', () => {
     }).compile();
 
     expect(moduleRef.get(WarehouseService)).toBeInstanceOf(WarehouseService);
+  });
+
+  it('loads decorated warehouse items during Nest application bootstrap', async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [WarehouseModule.forRoot()],
+      providers: [IntegrationLoader],
+    }).compile();
+
+    await moduleRef.init();
+
+    const warehouse = moduleRef.get(WarehouseService);
+
+    expect(warehouse.isReady()).toBe(true);
+    expect(warehouse.get('countries')).toEqual({ CZ: 'Czech Republic' });
+
+    await moduleRef.close();
   });
 });
